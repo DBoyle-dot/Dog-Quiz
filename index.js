522b424b5d376c2e5f9cb2c8cecfe797122b41fd
DBoyle <dboyle091@gmail.com>
2024-12-22 12:17:05 +0000

    update

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const express = require('express')
+const http = require('http')
+const app = express();
+const port = 5500;
+const bodyParser = require('body-parser')
+const path = require("path")
 
+
+
+app.use(bodyParser.json())
+app.use(express.static(__dirname))
+
+app.get("/", (_req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+})
+
+app.listen(port, () => { 
+    console.log(`listening on port ${port}, http://localhost:${port}`); 
+}); 
